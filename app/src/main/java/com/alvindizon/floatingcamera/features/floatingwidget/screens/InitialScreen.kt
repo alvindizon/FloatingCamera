d0a5735e46bcc8471ee7919d77bf834c413141e1
Alvin Dizon <alvin.dizon91@gmail.com>
2022-07-31 09:56:36 +0800

    Add initial screen for launching floating camera
    Signed-off-by: Alvin Dizon <alvin.dizon91@gmail.com>

---
+++ app/src/main/java/com/alvindizon/floatingcamera/features/floatingwidget/screens/InitialScreen.kt
@@ -0,0 +1,104 @@
+package com.alvindizon.floatingcamera.features.floatingwidget.screens
+
+import android.content.Intent
+import android.net.Uri
+import android.provider.Settings
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.AlertDialog
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.alvindizon.floatingcamera.R
+
+@Composable
+fun InitialScreen() {
+    val context = LocalContext.current
+
+    val showDialog = rememberSaveable { mutableStateOf(false) }
+    val isPermissionGranted = rememberSaveable { mutableStateOf(false) }
+
+    if (showDialog.value) {
+        PermissionDialog(
+            onDismiss = { showDialog.value = false },
+            onOkClick = {
+                context.startActivity(
+                    Intent(
+                        Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                        Uri.parse("package:${context.packageName}")
+                    )
+                )
+            }
+        )
+    }
+
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .padding(8.dp)) {
+        InitialScreenContent(modifier = Modifier.align(Alignment.Center)) {
+            isPermissionGranted.value = Settings.canDrawOverlays(context)
+            // show dialog if app can't draw over other apps
+            showDialog.value = !isPermissionGranted.value
+        }
+    }
+
+}
+
+@Composable
+fun InitialScreenContent(modifier: Modifier = Modifier, onButtonClick: () -> Unit) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            textAlign = TextAlign.Center,
+            text = stringResource(id = R.string.initial_screen_content_title),
+            style = MaterialTheme.typography.subtitle1
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Button(onClick = onButtonClick) {
+            Text(
+                text = stringResource(id = R.string.initial_screen_content_button_msg),
+                style = MaterialTheme.typography.subtitle2
+            )
+        }
+    }
+}
+
+@Composable
+fun PermissionDialog(
+    onDismiss: () -> Unit,
+    onOkClick: () -> Unit
+) {
+    AlertDialog(
+        onDismissRequest = onDismiss,
+        confirmButton = {
+            TextButton(
+                onClick = {
+                    onOkClick.invoke()
+                    onDismiss.invoke()
+                }
+            ) {
+                Text(text = stringResource(id = R.string.initial_screen_button_label))
+            }
+        },
+        title = { Text(text = stringResource(id = R.string.initial_screen_dialog_title)) },
+        text = { Text(text = stringResource(id = R.string.initial_screen_permission_msg)) }
+    )
+}
